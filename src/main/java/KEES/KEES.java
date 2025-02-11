220cd291eeb928905d26f7b2ca71b0eda39238b9
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2025-02-11 13:41:04 +0200

    gui main setup

--- src/main/java/KEES/KEES.java
+++ src/main/java/KEES/KEES.java
@@ -1,6 +1,4 @@
 package KEES;
-import java.sql.Connection;
-import java.util.Scanner;
 
 /**
  *
@@ -10,29 +8,27 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 public class KEES {
 
-    @SuppressWarnings("ConvertToTryWithResources")
+    // CLI Main
+    // public static void main(String[] args) {
+    //     try (Connection conn = DataBase.CreateTabelesOneTime()) {
+    //         Scanner input = new Scanner(System.in);   
+    //         System.out.println("welcome to KEES Prototype:");
+    //         System.out.println("1. add Question");
+    //         System.out.println("2. create exam");
+    //         int choice = Integer.parseInt(input.nextLine());   
+    //         if(choice == 1) {
+    //             boolean isSuccess = DataBase.addQuestionsFromFile("test.txt", conn, input);
+    //         } else if(choice == 2) {
+    //             boolean isSuccess = DataBase.createExamWithConditions("testExam.txt", conn, input);     
+    //         }
+    //         input.close();
+    //     } catch (Exception e) {
+    //         System.out.println(e);
+    //     }
+    // }
+
+    // GUI Main
     public static void main(String[] args) {
-
-        try (Connection conn = DataBase.CreateTabelesOneTime()) {
-            Scanner input = new Scanner(System.in);
-            
-            System.out.println("welcome to KEES Prototype:");
-            System.out.println("1. add Question");
-            System.out.println("2. create exam");
-            int choice = Integer.parseInt(input.nextLine());
-            
-            if(choice == 1) {
-                boolean isSuccess = DataBase.addQuestionsFromFile("test.txt", conn, input);
-
-            } else if(choice == 2) {
-                boolean isSuccess = DataBase.createExamWithConditions("testExam.txt", conn, input);
-                
-            }
-            
-            input.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        
     }
 }
